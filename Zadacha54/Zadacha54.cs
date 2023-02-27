4eae5d8e543498494002bf266ed6f8f514dea151
AlexIv <ivlieva.medphyslab.com>
2023-02-27 19:49:07 +0300

    Code to make an array added

--- Zadacha54/Zadacha54.cs
+++ Zadacha54/Zadacha54.cs
@@ -7,3 +7,38 @@
 7 4 2 1
 9 5 3 2
 8 4 4 2*/
+System.Console.WriteLine("Enter number of rows: "); 
+int M = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter number of columns: "); 
+int N = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter maximal number: "); 
+int MaxValue = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter minimal number: "); 
+int MinValue = Int32.Parse(System.Console.ReadLine());
+int [,] Matrix= Get2DArray(M,N,MaxValue,MinValue);
+Print2DArray (Matrix);
+
+int [,] Get2DArray (int M, int N, int MaxValue, int MinValue)
+{
+int[,] Array2D = new int [M,N];
+for (int i=0; i<Array2D.GetLength(0); i++)
+ {
+    for (int j=0; j<Array2D.GetLength(1); j++)
+    {
+      Array2D[i,j]= new Random().Next(MinValue, MaxValue+1);
+    }
+ }
+ return Array2D;
+}
+
+void Print2DArray (int [,] Matrix)
+{
+for (int i=0; i<Matrix.GetLength(0); i++)
+ {
+    for (int j=0; j<Matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{Matrix[i,j]} ");
+    }
+    System.Console.WriteLine();
+ }
+}
