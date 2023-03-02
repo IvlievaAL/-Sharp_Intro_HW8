6f0a2c41a2c9cca7a0eb890d29b7950721618b9c
AlexIv <ivlieva.medphyslab.com>
2023-03-02 22:39:25 +0300

    Code to make 2 initial 2D arrays added

--- Zadacha58/Zadacha58.cs
+++ Zadacha58/Zadacha58.cs
@@ -16,3 +16,58 @@ AB[i,j] = сумма произведений элементов i-той стр
 2. Проверка возможности их перемножения;
 3. Собственно их перемножение;
 4. Вывод трех матриц в консоль;*/
+int [,] MatrixA= Get2DArrayFromConsole (); //Создание двух матриц по вводимым с консоли параметрам
+int [,] MatrixB= Get2DArrayFromConsole ();
+//int [,] MatrixAB= вот сюда наша функция; //Проверка возможности их перемножения+Собственно их перемножение.
+int [,] Matrix = MatrixA; //Вывод трех матриц в консоль.
+Print2DArray (Matrix);
+System.Console.WriteLine(); // отступ на 1 строку
+Matrix = MatrixB;
+Print2DArray (Matrix);
+System.Console.WriteLine(); // отступ на 1 строку
+//int [,] Matrix = MatrixAB;
+//Print2DArray (Matrix);
+
+
+// Список функций.
+
+
+
+
+
+
+
+
+
+int [,] Get2DArrayFromConsole ()
+{
+System.Console.WriteLine("Enter number of rows: ");
+int M = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter number of columns: "); 
+int N = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter maximal number: "); 
+int MaxValue = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter minimal number: "); 
+int MinValue = Int32.Parse(System.Console.ReadLine());
+int[,] Array2D = new int [M,N];
+for (int i=0; i<Array2D.GetLength(0); i++)
+ {
+    for (int j=0; j<Array2D.GetLength(1); j++)
+    {
+      Array2D[i,j]= new Random().Next(MinValue, MaxValue+1);
+    }
+ }
+ return Array2D;
+}
+
+void Print2DArray (int [,] Matrix)
+{
+for (int i=0; i<Matrix.GetLength(0); i++)
+ {
+    for (int j=0; j<Matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{Matrix[i,j]} ");
+    }
+    System.Console.WriteLine();
+ }
+}
