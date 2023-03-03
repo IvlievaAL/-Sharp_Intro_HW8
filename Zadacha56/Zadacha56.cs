9910b15496a9e2a2118a644abb5287d085ad39f4
AlexIv <ivlieva.medphyslab.com>
2023-03-03 11:53:11 +0300

    Code to get any array from console added

--- Zadacha56/Zadacha56.cs
+++ Zadacha56/Zadacha56.cs
@@ -11,3 +11,40 @@
 5 2 6 7
 
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int [,] Matrix= Get2DArrayFromConsole ();
+Print2DArray (Matrix);
+System.Console.WriteLine(); // отступ на 1 строку
+
+int [,] Get2DArrayFromConsole ()
+{
+System.Console.WriteLine("Enter number of rows: ");
+int M = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter number of columns: "); 
+int N = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter maximal number: "); 
+int MaxValue = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter minimal number: "); 
+int MinValue = Int32.Parse(System.Console.ReadLine());
+int[,] Array2D = new int [M,N];
+for (int i=0; i<Array2D.GetLength(0); i++)
+ {
+    for (int j=0; j<Array2D.GetLength(1); j++)
+    {
+      Array2D[i,j]= new Random().Next(MinValue, MaxValue+1);
+    }
+ }
+ return Array2D;
+}
+
+void Print2DArray (int [,] Matrix)
+{
+for (int i=0; i<Matrix.GetLength(0); i++)
+ {
+    for (int j=0; j<Matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{Matrix[i,j]} ");
+    }
+    System.Console.WriteLine();
+ }
+}
